dc97dd3a00c81ffb6a84722135421526e87b1340
eziominh <eziominh@gmail.com>
2017-09-29 11:35:02 +0700

    Bai 1 + 2

---
+++ Bai1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Bai1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Get local host name
+            String StrHostName = string.Empty;
+            StrHostName = Dns.GetHostName();
+            Console.WriteLine("Ten may: " + StrHostName);
+            //Get IP Address
+            IPHostEntry ipEntry = Dns.GetHostEntry(StrHostName);
+            IPAddress[] addr = ipEntry.AddressList;
+            foreach (IPAddress ip4 in addr.Where(ip => ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork))
+            {
+                Console.WriteLine(ip4.ToString());
+            }  
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Bai2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Bai2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Domain to IP
+            string Host;
+            Console.WriteLine("Input domain name:");
+            Host = Console.ReadLine();
+
+            foreach (IPAddress ip in Dns.GetHostAddresses(Host))
+            {
+                Console.WriteLine("Domain's IP Address:" +ip.ToString());
+            }
+            //IP to Domain
+            string IPAdd;
+            Console.WriteLine("Input IP Address: ");
+            IPAdd = Console.ReadLine();
+
+            foreach (IPHostEntry ip in Dns.GetHostByName(IPAdd))
+            {
+                Console.WriteLine("Domain's name:" + ip.ToString());
+            }
+
+        }
+    }
+}
